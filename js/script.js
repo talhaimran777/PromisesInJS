8a17cbec402a17327776cc6a008772991d6cec6c
Talha Imran <talhaimran284@gmail.com>
2020-08-15 11:38:31 +0500

    Example From Web Dev Simplified

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-alert("Hello");
+/* alert("Hello"); */
+
+let weather = false;
+
+/* const downloadImg = () =>{
+
+} */
+
+let date = new Promise((resolve, reject) =>{
+
+
+ /*    setTimeout(downloadImg, 1500); */
+    if(weather){
+        resolve({name: "Mcdonalds", location: "Lahore", table: 5,});
+    }
+    else{
+        reject({reason: "Bad Weather", result: "No dates"});
+    }
+});
+
+
+date.then((x) =>{
+    console.log("In Then block");
+    console.log(x);
+}).catch((x) =>{
+    console.log("In Catch block");
+    console.log(x);
+});
